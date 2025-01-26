e3063d23fc9799703be3c25d4d9cc353d927f8c6
Angelo Pirola <angelo9871@gmail.com>
2025-01-26 01:02:06 +0100

    Refactoring

--- src/Shared/DependencyInjection.cs
+++ src/Shared/DependencyInjection.cs
@@ -1,70 +1,103 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Net.Http.Headers;
 using Microsoft.OpenApi.Models;
-using System.Text;
 
 namespace Shared;
 
 public static class DependencyInjection
 {
-	public static IServiceCollection AddSwaggerAuth(this IServiceCollection services)
-	{
-		services.AddEndpointsApiExplorer();
-		services.AddSwaggerGen(options =>
-		{
-			options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-			{
-				In = ParameterLocation.Header,
-				Description = "Insert the Bearer Token",
-				Name = HeaderNames.Authorization,
-				Type = SecuritySchemeType.ApiKey
-			});
+    public static IServiceCollection AddSwaggerAuth(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Insert the Bearer Token",
+                Name = HeaderNames.Authorization,
+                Type = SecuritySchemeType.ApiKey
+            });
 
-			options.AddSecurityRequirement(new OpenApiSecurityRequirement
-			{
-				{
-					new OpenApiSecurityScheme
-					{
-						Reference= new OpenApiReference
-						{
-							Type = ReferenceType.SecurityScheme,
-							Id = JwtBearerDefaults.AuthenticationScheme
-						}
-					},
-					Array.Empty<string>()
-				}
-			});
-		});
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference= new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = JwtBearerDefaults.AuthenticationScheme
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+        });
 
-		return services;
-	}
+        return services;
+    }
 
-	public static IServiceCollection AddAuthenticationJWT(this IServiceCollection services, IConfiguration configuration)
-	{
-		services.AddAuthentication(options =>
-		{
-			options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-			options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-		})
-		.AddJwtBearer(options =>
-		{
-			options.TokenValidationParameters = new TokenValidationParameters
-			{
-				ValidateIssuer = true,
-				ValidateAudience = true,
-				ValidateLifetime = true,
-				ValidateIssuerSigningKey = true,
-				ValidIssuer = configuration["Jwt:Issuer"],
-				ValidAudience = configuration["Jwt:Audience"],
-				IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
-			};
-		});
+    public static IServiceCollection AddAuthenticationJWT(this IServiceCollection services)
+    {
+        services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
 
-		services.AddAuthorization();
+            // Criteri di validazione della password
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequiredUniqueChars = 4;
 
-		return services;
-	}
+            // Conferma dell'account
+            options.SignIn.RequireConfirmedEmail = true;
+
+            // Blocco dell'account
+            options.Lockout.AllowedForNewUsers = true;
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+        // Configurazione di JWT
+        services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = JWTConfiguration.Issuer,
+                ValidAudience = JWTConfiguration.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWTConfiguration.SecurityKey)),
+                RequireExpirationTime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+        services.AddScoped<IAuthorizationHandler, UserActiveHandler>();
+        services.AddAuthorization(options =>
+        {
+            var policyBuilder = new AuthorizationPolicyBuilder().RequireAuthenticatedUser();
+            policyBuilder.Requirements.Add(new UserActiveRequirement());
+            options.FallbackPolicy = options.DefaultPolicy = policyBuilder.Build();
+        });
+
+        return services;
+    }
 }
