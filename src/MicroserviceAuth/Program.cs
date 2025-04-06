2d304f83405fddd43afc2bb824a73c500204b595
Angelo Pirola <angelo9871@gmail.com>
2025-04-06 18:15:41 +0200

    Update package versions and refactor Program.cs
    Updated NuGet package references in `MicroserviceAuth.csproj` to newer versions for authentication, identity, and Entity Framework Core. Improved code formatting and structure in `Program.cs`, including corrected indentation and enhanced readability for the JWT token creation logic.

--- src/MicroserviceAuth/Program.cs
+++ src/MicroserviceAuth/Program.cs
@@ -6,57 +6,56 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Shared;
 
-namespace MicroserviceAuth
+namespace MicroserviceAuth;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSwaggerAuth();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("IdentityMemoryDatabase"));
+
+        builder.Services.AddAuthenticationJWT();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-            builder.Services.AddSwaggerAuth();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseInMemoryDatabase("IdentityMemoryDatabase"));
-
-            builder.Services.AddAuthenticationJWT();
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
+        app.MapPost("/login", async (LoginRequest request, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) =>
+        {
+            if (request.Username == "admin" && request.Password == "admin")
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(JWTConfiguration.SecurityKey);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity([
+                        new(ClaimTypes.NameIdentifier, "aa787598-2faa-4d8d-b102-2b5f5de364c8"),
+                        new(ClaimTypes.Name, "admin")
+                    ]),
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = JWTConfiguration.Issuer,
+                    Audience = JWTConfiguration.Audience
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                return Results.Ok(new { Token = tokenHandler.WriteToken(token) });
             }
 
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapPost("/login", async (LoginRequest request, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) =>
-            {
-                if (request.Username == "admin" && request.Password == "admin")
-                {
-                    var tokenHandler = new JwtSecurityTokenHandler();
-                    var key = Encoding.UTF8.GetBytes(JWTConfiguration.SecurityKey);
-                    var tokenDescriptor = new SecurityTokenDescriptor
-                    {
-                        Subject = new ClaimsIdentity([
-                            new(ClaimTypes.NameIdentifier, "aa787598-2faa-4d8d-b102-2b5f5de364c8"),
-                            new(ClaimTypes.Name, "admin")
-                        ]),
-                        Expires = DateTime.UtcNow.AddHours(1),
-                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                        SecurityAlgorithms.HmacSha256Signature),
-                        Issuer = JWTConfiguration.Issuer,
-                        Audience = JWTConfiguration.Audience
-                    };
-
-                    var token = tokenHandler.CreateToken(tokenDescriptor);
-                    return Results.Ok(new { Token = tokenHandler.WriteToken(token) });
-                }
-
-                return Results.Unauthorized();
-            }).AllowAnonymous();
-        }
+            return Results.Unauthorized();
+        }).AllowAnonymous();
     }
 }
