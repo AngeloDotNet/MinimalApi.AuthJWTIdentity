2442a47e35124b3a3e4dc0c820320bd254ef7c0a
Angelo Pirola <angelo9871@gmail.com>
2025-04-06 18:15:34 +0200

    Update package versions and improve Program.cs formatting
    Updated `Microservice02.csproj` to upgrade several package references to their latest versions, including `Microsoft.AspNetCore.Identity.EntityFrameworkCore`, `Microsoft.EntityFrameworkCore.InMemory`, `Microsoft.AspNetCore.Authentication.JwtBearer`, `Microsoft.AspNetCore.OpenApi`, and `Swashbuckle.AspNetCore`.
    
    Refactored `Program.cs` for better readability by modifying the namespace declaration and restructuring the `Main` method to use braces for scope definition, enhancing clarity and maintainability while preserving the original logic.

--- src/Microservice02/Program.cs
+++ src/Microservice02/Program.cs
@@ -2,35 +2,34 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Shared;
 
-namespace Microservice02
+namespace Microservice02;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSwaggerAuth();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseInMemoryDatabase("IdentityMemoryDatabase"));
+
+        builder.Services.AddAuthenticationJWT();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddSwaggerAuth();
-
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseInMemoryDatabase("IdentityMemoryDatabase"));
-
-            builder.Services.AddAuthenticationJWT();
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapGet("/secure", [Authorize] () => "This is a secure endpoint");
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapGet("/secure", [Authorize] () => "This is a secure endpoint");
+
+        app.Run();
     }
 }
